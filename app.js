bb9b8411d3244f9ad84f5d0b6dbc0ca9f2a4668c
Nuri <dagasannuri@gmail.com>
2022-01-12 15:12:39 +0000

    added CRUD functionality in views folder for campsites

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const url = 'mongodb://localhost:27017/n-camp';
 const Campsite = require('./models/campsite');
 
@@ -16,18 +17,59 @@ db.once('open', () => {
 const app = express();
 
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'))
+app.set('views',path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(methodOverride('_method'));
 
 app.get('/',(req,res) => {
     res.render('index');
 })
 
-app.get('/makecampsite', async(req,res) => {
-    const camp = new Campsite({title:'Bosphorus', price:'249', description:'Beautiful see view', location:'Istanbul'});
-    await camp.save();
-    res.send(camp);
+app.get('/campsites', async (req,res) => {
+    const campsites = await Campsite.find({});
+    res.render('campsites/index',{campsites});
+
+});
+
+app.get('/campsites/new', (req,res) => {
+    res.render('campsites/new');
+});
+
+app.post('/campsites', async (req,res) => {
+    const {title, description, location} = req.body.campsite;
+    const campsite = new Campsite(
+        {title:title,description:description,location:location});
+    await campsite.save();
+    res.redirect('campsites');
+});
+
+app.get('/campsites/:id', async (req,res) => {
+    const {id} = req.params;
+    const campsite = await Campsite.findById(id);
+    res.render('campsites/show',{campsite});
+});
+
+app.get('/campsites/:id/edit', async (req,res) => {
+    const {id} = req.params;
+    const campsite = await Campsite.findById(id);
+    res.render('campsites/edit',{campsite});
+});
+
+app.put('/campsites/:id',async(req,res) => {
+    const {id} = req.params;
+    const {title,location,description} = req.body.campsite;
+    await Campsite.findByIdAndUpdate({_id:id}
+        ,{title:title,location:location,description:description});
+    res.redirect(`/campsites/${id}`);
 })
 
+app.delete('/campsites/:id',async(req,res)=> {
+    const {id} = req.params;
+    await Campsite.findOneAndDelete({_id:id});
+    res.redirect('/campsites');
+})
 
 app.listen(3000,()=> {
     console.log("PORT 3000, LISTENING!");
