8ba99230bfe7184a94669a28c7d2e3a4588c51b2
Nuri <dagasannuri@gmail.com>
2022-01-16 16:05:10 +0000

    added layouts and partials. formatted ejs templates

--- app.js
+++ app.js
@@ -1,76 +1,89 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate')
 const methodOverride = require('method-override');
 const url = 'mongodb://localhost:27017/n-camp';
 const Campsite = require('./models/campsite');
+const morgan = require('morgan');
 
 mongoose.connect(url);
 
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Database Connected.')
 });
 
 const app = express();
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
-app.set('views',path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(methodOverride('_method'));
-
-app.get('/',(req,res) => {
-    res.render('index');
+app.use(morgan('tiny'));
+app.use((req, res, next) => {
+    console.log("MIDDLEWARE");
+    return next();
 })
 
-app.get('/campsites', async (req,res) => {
+app.get('/', (req, res) => {
+    res.send('index');
+})
+
+app.get('/campsites', async (req, res) => {
     const campsites = await Campsite.find({});
-    res.render('campsites/index',{campsites});
+    res.render('campsites/index', { campsites });
 
 });
 
-app.get('/campsites/new', (req,res) => {
+app.get('/campsites/new', (req, res) => {
     res.render('campsites/new');
 });
 
-app.post('/campsites', async (req,res) => {
-    const {title, description, location} = req.body.campsite;
+app.post('/campsites', async (req, res) => {
+    const { title, description, location } = req.body.campsite;
     const campsite = new Campsite(
-        {title:title,description:description,location:location});
+        { title: title, description: description, location: location });
     await campsite.save();
     res.redirect('campsites');
 });
 
-app.get('/campsites/:id', async (req,res) => {
-    const {id} = req.params;
+app.get('/campsites/:id', async (req, res) => {
+    const { id } = req.params;
     const campsite = await Campsite.findById(id);
-    res.render('campsites/show',{campsite});
+    res.render('campsites/show', { campsite });
 });
 
-app.get('/campsites/:id/edit', async (req,res) => {
-    const {id} = req.params;
+app.get('/campsites/:id/edit', async (req, res) => {
+    const { id } = req.params;
     const campsite = await Campsite.findById(id);
-    res.render('campsites/edit',{campsite});
+    res.render('campsites/edit', { campsite });
 });
 
-app.put('/campsites/:id',async(req,res) => {
-    const {id} = req.params;
-    const {title,location,description} = req.body.campsite;
-    await Campsite.findByIdAndUpdate({_id:id}
-        ,{title:title,location:location,description:description});
+app.put('/campsites/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, location, description } = req.body.campsite;
+    await Campsite.findByIdAndUpdate({ _id: id }
+        , { title: title, location: location, description: description });
     res.redirect(`/campsites/${id}`);
 })
 
-app.delete('/campsites/:id',async(req,res)=> {
-    const {id} = req.params;
-    await Campsite.findOneAndDelete({_id:id});
+app.delete('/campsites/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campsite.findOneAndDelete({ _id: id });
     res.redirect('/campsites');
 })
 
-app.listen(3000,()=> {
+app.use((req, res) => {
+    res.status(404).send('NOT FOUND 404!')
+})
+
+
+app.listen(3000, () => {
     console.log("PORT 3000, LISTENING!");
 })
--- dbSeeder/index.js
+++ dbSeeder/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const data = require('./data')
-const {descriptors,places} = require('./seedHelper')
+const { descriptors, places } = require('./seedHelper')
 const url = 'mongodb://localhost:27017/n-camp';
 const Campsite = require('../models/campsite');
 
@@ -8,26 +8,26 @@ mongoose.connect(url);
 
 const db = mongoose.connection;
 
-db.on('error',console.error.bind(console,'connection error:'));
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log('Database Connected.')
 });
 
-const random = (array) => array[Math.floor(Math.random()*array.length)];
+const random = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedCampsiteDB = async () => {
     await Campsite.deleteMany({});
-    for(let i = 0; i<100;i++) {
+    for (let i = 0; i < 100; i++) {
         const randi = Math.floor(Math.random() * 1000);
         const camp = new Campsite({
             title: `${random(descriptors)} ${random(places)}`,
-            location: `${data[randi].city}, ${data[randi].state}`  
+            location: `${data[randi].city}, ${data[randi].state}`
         });
         await camp.save();
     }
-    
+
 };
 
-seedCampsiteDB().then(()=> {
+seedCampsiteDB().then(() => {
     mongoose.connection.close();
 });
